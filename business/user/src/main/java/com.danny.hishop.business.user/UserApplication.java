f2e1c3cf657e085bc4558a46ed098c9943b7906a
dannyhoo <yuyang.hu@opay-inc.com>
2019-11-26 18:21:20 +0800

    修改数据库配置

---
+++ business/user/src/main/java/com.danny.hishop.business.user/UserApplication.java
@@ -0,0 +1,26 @@
+package com.danny.hishop.business.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author huyuyang
+ * @email yuyang.hu@opay-inc.com
+ * @date 2019/11/26下午5:50
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients //启用feign客户端
+@EnableHystrixDashboard //启动hystrix控制台
+@EnableCircuitBreaker //启动断路器
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
