66c1eae194bee8ee254e3fc06c32d2eccf245fbc
dannyhoo <yuyang.hu@opay-inc.com>
2020-01-19 16:10:15 +0800

    日志添加唯一标识

---
+++ business/aggregation/src/main/java/com/danny/hishop/business/aggregation/config/WebConfigurer.java
@@ -0,0 +1,25 @@
+package com.danny.hishop.business.aggregation.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfigurer implements WebMvcConfigurer {
+
+    @Autowired
+    private LogInterceptor logInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+    }
+}
